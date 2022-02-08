db3695bed93ad08be1f687a04491dd747b4fa6c9
Eloi <92112623+EloiBienert@users.noreply.github.com>
2022-02-08 20:11:29 +0100

    Commit

--- 05.ps1
+++ 05.ps1
@@ -1,2 +1,3 @@
 #Assigna a la variable $a el Id del procés que té per nom 'bash'
-$a = Get-Process bash
+$a = Get-WmiObject -Class Win32_Service -Filter "Name LIKE 'bash'" | 
+      Select-Object -ExpandProperty ProcessId
