0f461cff0708f3d976b6e5f2e7ade7170de79756
Eloi <92112623+EloiBienert@users.noreply.github.com>
2022-02-08 20:13:49 +0100

    Commit

--- 05.ps1
+++ 05.ps1
@@ -1,3 +1,2 @@
 #Assigna a la variable $a el Id del procés que té per nom 'bash'
-$a = Get-WmiObject -Class Win32_Service -Filter "Name LIKE 'bash'" | 
-      Select-Object -ExpandProperty ProcessId
+$a = get-process -id winword
